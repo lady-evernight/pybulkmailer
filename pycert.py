c31b7c27fc77c5b91fd7fdce0d0969056791c1a4
Micaela Reyes <codemickeycode@gmail.com>
2015-07-28 19:24:58 +0800

    removed code not needed

--- pycert.py
+++ pycert.py
@@ -2,33 +2,11 @@ import csv, os
 import PyPDF2
 import StringIO
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import portrait, letter, landscape
+from reportlab.lib.pagesizes import portrait, letter
 
 filepath = os.path.abspath('data/nameslist.csv')
 
 class CertificateMaker():
-
-    def writeString(self):
-        # fpath="/home/micaela/Projects/pycert/templates/participants.pdf"
-        dumpfile = open('/home/micaela/Projects/pycert/templates/participants.pdf', 'rw+')
-        packet = StringIO.StringIO()
-        packet = StringIO.StringIO()
-        cv=canvas.Canvas(packet, pagesize=letter)
-        
-        #create a string
-        cv.drawString(250, 500, "Hello World!")
-        #save to string
-        cv.save()
-        
-        #get back to 0
-        # packet.seek(0)
-        
-        #write to a file
-        with open('output/testoutput.pdf','wb') as fp:
-            fp.write(packet.getvalue())
-
-        # outputfile = open('output/testoutput.pdf', 'wb')
-        # writer = pdf.PdfFileWriter()
 
     # https://automatetheboringstuff.com/chapter13/
     def write_string_to_pdf(self):
@@ -50,10 +28,6 @@ class CertificateMaker():
         pdfWatermarkReader = PyPDF2.PdfFileReader(open('output/testoutput.pdf', 'rb'))
         certFirstPage.mergePage(pdfWatermarkReader.getPage(0))
         pdfWriter.addPage(certFirstPage)
-
-        # for pageNum in range(pdf1Reader.numPages):
-        #     pageObj = pdf1Reader.getPage(pageNum)
-        #     pdfWriter.addPage(pageObj)
 
         pdfOutputFile = open('output/particpant-output.pdf', 'wb')
         pdfWriter.write(pdfOutputFile)
