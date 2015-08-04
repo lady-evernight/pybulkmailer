0cc138cb366e57a5b350c3ac71a0c10212f570e5
Micaela Reyes <codemickeycode@gmail.com>
2015-08-04 17:24:23 +0800

    I know, code looks like crap but its working! - certificate maker - email sender with pdf attachment

--- pycert.py
+++ pycert.py
@@ -4,12 +4,20 @@ import StringIO
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import portrait, letter, landscape
 
+# Import smtplib for the actual sending function
+import smtplib
+# Import the email modules we'll need
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+
 filepath = os.path.abspath('data/nameslist.csv')
+
 
 class CertificateMaker():
 
     # https://automatetheboringstuff.com/chapter13/
-    def write_string_to_pdf(self, namestring):
+    def write_string_to_pdf(self, participants_name):
         outputfiletemp = 'output/testoutput.pdf'
         pdf1File = open('templates/participants.pdf', 'rb')
         pdf1Reader = PyPDF2.PdfFileReader(pdf1File)
@@ -20,7 +28,7 @@ class CertificateMaker():
         cv=canvas.Canvas(packet, pagesize=letter)
         cv.setPageSize(landscape(letter))
         #create a string
-        cv.drawString(350, 300, namestring)
+        cv.drawString(350, 300, participants_name)
         #save to string
         cv.save()
         #write to a file
@@ -32,10 +40,36 @@ class CertificateMaker():
         certFirstPage.mergePage(pdfWatermarkReader.getPage(0))
         pdfWriter.addPage(certFirstPage)
 
-        pdfOutputFile = open('output/pyconph2015_certificate' + namestring + '.pdf', 'wb')
+        pdfOutputFile = open('output/pyconph2015_certificate_' + participants_name + '.pdf', 'wb')
         pdfWriter.write(pdfOutputFile)
         pdfOutputFile.close()
         pdf1File.close()
+
+
+class EmailSender():
+
+    def send_email(self, participants_name, participants_email):
+        msg = MIMEMultipart()
+        msg['Subject'] = participants_name + ", here's your PyConPH 2015 Certificate"
+        msg['From'] = "info@python.ph"
+        msg['To'] = participants_email
+        filename = 'pyconph2015_certificate_' + participants_name + '.pdf'
+        certfile = 'output/' + filename
+        pdfAttachment = MIMEApplication(open(certfile).read())
+        pdfAttachment.add_header('content-disposition', 'attachment', filename = filename)
+        text = MIMEMultipart('alternative')
+        msg.attach(text)
+        msg.attach(pdfAttachment)
+
+        username = #username here
+        password = #password here
+
+        s = smtplib.SMTP('smtp.gmail.com')
+        s.ehlo()
+        s.starttls()
+        s.login(username, password)
+        s.sendmail("info@python.ph", participants_email, msg.as_string())
+        s.quit()
 
 
 if __name__ == "__main__":
@@ -48,5 +82,6 @@ if __name__ == "__main__":
             cert = CertificateMaker()
             cert.write_string_to_pdf(row[0])
 
-    # TODO:
-    # send email to each participant
+            # send email to each participant
+            mail = EmailSender()
+            mail.send_email(row[0], row[1])
